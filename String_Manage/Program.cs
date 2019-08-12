8eab9c6cd145f6154b118ae1642e73c5fade509d
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-08-13 07:30:52 +0900

    問題定義用のプログラム作成

--- String_Manage/Program.cs
+++ String_Manage/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
-namespace String_Manage
+namespace Reflection_Sample
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
+
+        //  ============== ほぼ同じメソッドができてしまった ==================
+
+        /// <summary>
+        /// Playerの与えるダメージの計算
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="enemy"></param>
+        /// <returns></returns>
+        int CalcPlayerDamage(Player player, Enemy enemy)
+        {
+            return (player.Attack - enemy.Attack / 4) * 100 / 256;
+        }
+
+        /// <summary>
+        /// Enemyの与えるダメージの計算
+        /// </summary>
+        /// <param name="enemy"></param>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        int CalcEnemyDamage(Enemy enemy, Player player)
+        {
+            return (enemy.Attack - player.Attack / 4) * 100 / 256;
+        }
+        //  =============================================================
+    }
+
+    class Player
+    {
+        public int HP;
+        public int Attack;
+        public int EXP;
+    }
+
+    class Enemy
+    {
+        public int HP;
+        public int Attack;
+        public int EXP;
+        public int DropItem;
     }
 }
