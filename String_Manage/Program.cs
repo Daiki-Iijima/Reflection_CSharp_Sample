774c80fd7d9605a108b49bb18ff1ff964eb6d3c3
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-08-13 07:13:34 +0900

    First Commit

---
+++ String_Manage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_Manage
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
